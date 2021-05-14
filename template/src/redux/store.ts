c45638a1849134cd64a1c01a673a8c648ff4e9e7
Henrique Marques <marquesprogrammer@hotmail.com>
2021-05-13 21:48:49 -0300

    chore: changed redux and removed reacttron

--- template/src/redux/store.ts
+++ template/src/redux/store.ts
@@ -1,25 +1,15 @@
-import { configureStore, Action, getDefaultMiddleware } from "@reduxjs/toolkit"
-import { combineEpics, createEpicMiddleware, Epic } from "redux-observable"
-import reactotron from "../../reactotron"
-import sentryMiddleware from "./middleware/sentryMiddleware"
-import { rootReducer, RootStoreType } from "./rootReducer"
-import { exampleEpics } from "./slices/exampleSlice"
+import { configureStore, Action, getDefaultMiddleware } from '@reduxjs/toolkit';
 
-// Redux observable
-export type MyEpic = Epic<any, any, RootStoreType, any>
-export const rootEpic = combineEpics(...exampleEpics)
-const epicMiddleware = createEpicMiddleware<any, any, RootStoreType, any>()
+import sentryMiddleware from './middleware/sentryMiddleware';
+import { rootReducer } from './rootReducer';
 
 const store = configureStore({
   reducer: rootReducer,
-  middleware: [epicMiddleware, sentryMiddleware, ...getDefaultMiddleware()],
-  // Only create reactotron enhancer in DEV
-  enhancers: (__DEV__ && [reactotron.createEnhancer()]) || undefined,
+  middleware: [sentryMiddleware, ...getDefaultMiddleware()],
+
   devTools: true,
-})
+});
 
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
 
-epicMiddleware.run(rootEpic)
-
-export default store
+export default store;
